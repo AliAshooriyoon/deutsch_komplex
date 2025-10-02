9bea6385584704ea73243c536aa11a13f1ed09b5
Ali Ashooriyoon <aliashooriyoon@yahoo.com>
2025-10-02 23:01:25 +0330

    header

---
+++ app/components/Header.tsx
@@ -0,0 +1,24 @@
+"use client"
+import logo from "@/public/de-icon.png"
+import Image from "next/image";
+const Header = () => {
+  return <>
+    <div className="w-full h-20 bg-[#4a4b4e] rounded-b-3xl flex items-center justify-between">
+      <div className="logo_box h-full gap-4 flex items-center pl-4">
+        <Image className="h-[80%] w-17" alt="logo" src={logo} />
+        <p className="logo_title text-2xl font-bold">Deutsch komplex</p>
+      </div>
+      <div className="items_header flex w-[40%] text-xl justify-around pr-6">
+        <div className="">Haus</div>
+        <div className="">Anfangen</div>
+        <div className="">Preise</div>
+        <div className="">Über uns</div>
+      </div>
+      <div className="buttons_box flex gap-8 pr-8 text-xl">
+        <button type="button" className="">registieren</button>
+        <button type="button" className="border-4 text-yellow-500 py-2 px-5 rounded-3xl border-yellow-500">Einloggen</button>
+      </div>
+    </div>
+  </>
+}
+export default Header;
