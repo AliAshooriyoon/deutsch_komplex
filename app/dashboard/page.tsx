2b1eaf48a02c3dc945a10e865b915637dd81ddcb
Ali Ashooriyoon <aliashooriyoon@yahoo.com>
2025-10-03 15:50:13 +0330

    design menu dashbord new

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,71 +1,92 @@
+
+import "./dashboardStyle.css"
+import { MdEmail } from "react-icons/md";
 import Image from "next/image";
 import { FcAbout } from "react-icons/fc";
+import { MdReport } from "react-icons/md";
+import { MdAccountCircle } from "react-icons/md";
+import { IoSettingsSharp } from "react-icons/io5";
+import { VscSymbolKeyword } from "react-icons/vsc";
 import { FaHeadphones } from "react-icons/fa";
 import { FaBookReader, FaPenAlt } from "react-icons/fa";
 import Logo from "../../public/de-icon.png";
 
 const Dashboard = () => {
   return (
-    <div className="dashboard_menu fixed left-0 top-0 bg-[#4a4b4e] px-4 rounded-r-3xl h-screen flex flex-col">
-      <div className="logoBox flex items-center gap-3 text-2xl py-4 pl-4 shrink-0">
+    <div className="dashboard_menu fixed left-0 top-0 bg-[#4a4b4e] px-4 pl-6 rounded-r-3xl h-screen flex flex-col">
+      <div className="logoBox flex items-center gap-3 text-2xl py-4  shrink-0">
         <Image alt="" src={Logo} />
         <p>Deutsch komplex</p>
       </div>
 
-      <div className="items flex-1 min-h-0 flex flex-col gap-12 overflow-y-auto pr-2">
-        <div className="main_item pl-1">
-          <p className="text-xl flex items-center gap-2">
+      <div className="items flex-1 min-h-0 flex flex-col gap-8 overflow-y-auto pr-2">
+        <div className='wrapper'></div>
+        <div className="main_item">
+          <p className="showItem flex items-center">
             <FaPenAlt />
-            Schreiben
+            <span>Schreiben</span>
           </p>
-          <ul className="indent-4 text-lg leading-12">
-            <li>Texte durch AI korrigieren</li>
-            <li>Tipps zum Schreiben</li>
-            <li>Beispiele zum Schreiben</li>
-            <li>Test-Modelle zum Schreiben</li>
-          </ul>
+
         </div>
 
-        <div className="main_item pl-1">
-          <p className="text-xl flex items-center gap-2">
+        <div className="main_item">
+          <p className="showItem flex items-center">
             <FaBookReader />
-            Lesen
+            <span>Lesen</span>
           </p>
-          <ul className="indent-4 text-lg leading-12">
-            <li>Test-Modelle zum Lesen</li>
-            <li>Verschiedene Texte</li>
-            <li>Verschiedene Geschichten</li>
-          </ul>
+
         </div>
-        <div className="main_item pl-1">
-          <p className="text-xl flex items-center gap-2">
-            <FaPenAlt />
-            Wörter
+        <div className="main_item">
+          <p className="showItem flex items-center">
+            <VscSymbolKeyword />
+            <span>Wörter</span>
           </p>
-          <ul className="indent-4 text-lg leading-12">
-            <li>Wörter lernen</li>
-            <li>Lainter</li>
-            <li>Nomen-Verb Verbindung</li>
-            <li>Umgangssprachige Wörter</li>
-          </ul>
+
         </div>
 
-        <div className="main_item pl-1">
-          <p className="text-xl flex items-center gap-2">
+        <div className="main_item">
+          <p className="showItem flex items-center">
             <FaHeadphones />
-            Hören
+            <span> Hören</span>
           </p>
-          <ul className="indent-4 text-lg leading-12">
-            <li>Test-Modelle zum Hören</li>
-            <li>Hördateien</li>
-            <li>Podcaste auf Deutsch</li>
-          </ul>
+
         </div>
 
-        <div className="main_item pl-1 pb-10">
-          <p className="text-xl flex items-center gap-2">
-            <FcAbout />
-            Über uns</p>
+        <div className='account'>
+          <div className='wrapper' />
+          <div className="main_item pt-6">
+            <p className="showItem flex items-center">
+              <MdAccountCircle className="" />
+              <span>Konto</span>
+            </p>
+          </div>
+          <div className="main_item pt-6">
+            <p className="showItem flex items-center">
+              <IoSettingsSharp className="" />
+              <span>Einstellung</span>
+            </p>
+          </div>
+        </div>
+        <div className='contact'>
+          <div className='wrapper'></div>
+          <div className="main_item pt-6">
+            <p className="showItem flex items-center">
+              <FcAbout className="" />
+              <span>Über uns</span>
+            </p>
+          </div>
+          <div className="main_item pt-6">
+            <p className="showItem flex items-center">
+              <MdEmail />
+              <span>E-Mail</span>
+            </p>
+          </div>
+          <div className="main_item pt-6">
+            <p className="showItem flex items-center">
+              <MdReport />
+              <span>Berichten</span>
+            </p>
+          </div>
         </div>
       </div>
     </div>
