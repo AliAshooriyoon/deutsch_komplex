34f9836840a85e72e03274c18a7d0f2425879f22
Ali Ashooriyoon <aliashooriyoon@yahoo.com>
2025-10-05 00:58:50 +0330

    use prisma and auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import AuthProvider from "./auth/Provider";
 
 export const metadata: Metadata = {
   title: "Deutsch komplex",
@@ -15,7 +16,9 @@ export default function RootLayout({
     <html lang="en">
       <body
       >
-        {children}
+        <AuthProvider>
+          {children}
+        </AuthProvider>
       </body>
     </html>
   );
--- auth.ts
+++ auth.ts
@@ -1,6 +1,54 @@
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import { compare } from 'bcryptjs';
 import NextAuth from 'next-auth';
 import GitHub from 'next-auth/providers/github';
+import prisma from './prisma/client';
+import Credentials from 'next-auth/providers/credentials';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [GitHub],
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GitHub,
+    Credentials({
+      name: 'Credentials',
+      credentials: {
+        email: { label: 'Email', type: 'text' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials.password) return null;
+        const user = await prisma?.user.findUnique({
+          where: { email: credentials?.email as string },
+        });
+        if (!user || !user.password) return null;
+        const isValid = await compare(credentials.password as string, user.password);
+        if (!isValid) return null;
+        return { id: user.id, name: user.name, email: user.email };
+      },
+    }),
+  ],
+  session: { strategy: 'jwt' },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      if (!token.id && token.email) {
+        const userDb = await prisma.user.findUnique({
+          where: { email: user.email },
+        });
+        token.id = userDb;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.id as string;
+      }
+      return session;
+    },
+  },
+  pages: {
+    signIn: '/login',
+  },
 });
