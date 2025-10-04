07c04d0b3857005ce41937d4f761d41b6ba695ad
Ali Ashooriyoon <aliashooriyoon@yahoo.com>
2025-10-04 11:56:00 +0330

    wirte some xample in Beispiele zum Schreiben

--- app/dashboard/schreiben/example/page.tsx
+++ app/dashboard/schreiben/example/page.tsx
@@ -1,6 +1,62 @@
 const ExampleSchreiben = () => {
   return <>
     <div className="">
+      <div className='a1 goethe'>
+        <p className='text-xl'>
+          <p className='text-center text-2xl'>A1 Goethe</p>
+          Situation:
+          Du bist im Urlaub in München.
+          Schreib eine Postkarte an deinen Freund Jonas.
+          Sag:
+          Wo du bist
+          Wie es dir gefällt
+          Was du gemacht hast
+        </p>
+        <p className='text-2xl pt-12'>
+          <p className="text-xl pb-8">Beispiel:</p>
+          Hallo Jonas,
+          ich bin jetzt in München! Die Stadt ist sehr schön und es gibt viele alte Gebäude. Gestern war ich im Deutschen Museum, das war super interessant. Das Wetter ist sonnig und warm. Ich mache viele Fotos und esse oft Brezeln.
+          Viele Grüße
+          Ali
+        </p>
+      </div>
+
+      <div className='a1 pt-32'>
+        <p className='text-xl'>
+          <p className='text-center text-2xl'>A1 ÖSD</p>
+          Situation:
+          Du warst bei Familie Huber in Wien zu Besuch.
+          Schreib einen Brief und sag:
+          dass es schön war,
+          was dir gefallen hat,
+          wann du wiederkommst.
+        </p>
+        <p className='text-2xl pt-12'>
+          <p className="text-xl pb-8">Beispiel:</p>
+          Liebe Familie Huber,
+
+          vielen Dank für die schöne Zeit in Wien. Das Essen war sehr lecker und das Zimmer war sehr gemütlich. Mir hat der Spaziergang im Park sehr gefallen. Ich komme im Sommer wieder!
+
+          Viele Grüße
+          Ali        </p>
+      </div>
+
+      <div className='a1 pt-24'>
+        <p className='text-xl'>
+
+          <p className='text-center text-2xl'>A1 TELC</p>
+          Situation:
+          Du bist im Hotel. Das Zimmer ist laut.
+          Schreib eine kurze Nachricht an die Rezeption.
+        </p>
+        <p className='text-2xl pt-12'>
+          <p className="text-xl pb-8">Beispiel:</p>
+          Guten Tag,
+          mein Zimmer ist sehr laut, weil die Straße draußen ist. Können Sie mir bitte ein anderes Zimmer geben?
+          Vielen Dank
+          Ali
+        </p>
+      </div>
 
     </div>
   </>
