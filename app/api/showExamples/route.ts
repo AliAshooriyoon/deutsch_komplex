e69dc9f7526ea2ebf01392ab14ab1e5d01e50550
Ali Ashooriyoon <aliashooriyoon@yahoo.com>
2025-10-08 23:04:53 +0330

    write examples in lesen

---
+++ app/api/showExamples/route.ts
@@ -0,0 +1,11 @@
+import prisma from '@/prisma/client';
+import { NextResponse, type NextRequest } from 'next/server';
+export const POST = async (req: NextRequest) => {
+  const body = await req.json();
+  const res = await prisma.example.findMany({
+    where: {
+      type: body.type,
+    },
+  });
+  return NextResponse.json(res);
+};
--- app/dashboard/lesen/example/page.tsx
+++ app/dashboard/lesen/example/page.tsx
@@ -1,6 +1,40 @@
+"use client"
+import { useEffect, useState } from "react";
+
+
 const Texts = () => {
-  return <>
-    <div className=''>Texts</div>
-  </>
+  const [data, setData] = useState([{ id: "", exam: "", topic: "", content: "", level: "" }])
+  const getData = async () => {
+    const res = await fetch("/api/showExamples", {
+      method: "POST", body: JSON.stringify({ type: "lesen" })
+    })
+    const dataFiltered = await res.json()
+    setData(dataFiltered)
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+  return (
+    <>
+      <div className=''>
+        <div className="flex flex-col justify-around gap-16 w-[80%] mx-auto">
+          <div className='a1 goethe example_box'>
+            {data.map(i =>
+              <div key={i.id} className='text-xl p-12 border-2 border-blue-400 rounded-2xl'>
+                <p className='text-center text-3xl'>{i.level} {i.exam}</p>
+                <span className="situation"></span>
+                <p>{i.topic}</p>
+                <p>
+                  {i.content}
+                </p>
+              </div>)}
+          </div>
+        </div>
+      </div>
+    </>
+
+  )
 }
 export default Texts;
