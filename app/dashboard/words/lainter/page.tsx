d2c2d35d7fe898b1e03674a53d9352f79c769b75
Ali Ashooriyoon <aliashooriyoon@yahoo.com>
2025-10-13 22:27:27 +0330

    design lainter page

--- app/dashboard/words/lainter/page.tsx
+++ app/dashboard/words/lainter/page.tsx
@@ -1,7 +1,16 @@
 const Lainter = () => {
   return <>
     <div className=''>
-      <div className=""> Lainter </div>
+      <div className="">
+        <div className="lainter_box flex items-center justify-between flex-col w-[60%] 
+          mt-24 bg-sky-700 rounded-3xl h-[40rem] mx-auto ">
+          <div className="pt-24">
+            <p className="text-5xl text-center pt-12">der Wandel</p>
+            <p className="text-2xl text-center pt-6">In besonderen Haltungen wird es verwendet</p>
+          </div>
+          <p className="text-2xl text-center bg-emerald-700 px-6 py-4 rounded-3xl mb-16 cursor-pointer"> Antwort sehen </p>
+        </div>
+      </div>
 
     </div>
   </>
