b3e340a9ceb3e8072793a17af10a02a150c9f888
Ali Ashooriyoon <aliashooriyoon@yahoo.com>
2025-10-14 14:32:44 +0330

    develop tests part for lesen

--- app/dashboard/lesen/tests/page.tsx
+++ app/dashboard/lesen/tests/page.tsx
@@ -1,6 +1,37 @@
+'use client'
+import { useState } from "react"
 const TestsLesen = () => {
+  const [answer, setAnswer] = useState('')
+  const [level, setLevel] = useState('')
+  const createExam = async () => {
+    const res = await fetch("/api/createExam", {
+      method: "POST",
+      body: JSON.stringify({
+        message: `Create one german exam for lesen like ÖSD
+      in level ${level}. Write not answer the questions. exam should be like official exams `
+      })
+    })
+    const data = await res.json();
+    setAnswer(data.reply)
+  }
+
+
   return <>
-    <div className=''>Tests zum Lesen</div>
+    <div className=''>
+      <div className='text-2xl flex flex-col items-start gap-12'>
+        <p className="text-xl text-center py-12">{answer}</p>
+        <p className='text-2xl '>Eine Prüfung auswählen</p>
+        <select onChange={(e) => setLevel(e.target.value)}
+          className="w-32 border-2 py-2 px-5 rounded-2xl">
+          <option value={'a1'}>A1</option>
+          <option value={'a2'}>A2</option>
+          <option value={'b1'}>B1</option>
+          <option value={'b2'}>B2</option>
+          <option value={'c1'}>C1</option>
+        </select>
+        <button onClick={createExam} className="cursor-pointer border-2 p-2 rounded-2xl">Prüfung erstellen</button>
+      </div>
+    </div>
   </>
 }
 export default TestsLesen;
