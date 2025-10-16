ca571fa6344f6ae33eae784324bd4b67f31ae1c0
Ali Ashooriyoon <aliashooriyoon@yahoo.com>
2025-10-16 21:54:42 +0330

    show loading for words list

--- app/dashboard/words/words-list/page.tsx
+++ app/dashboard/words/words-list/page.tsx
@@ -4,15 +4,21 @@ import { useState } from "react"
 
 
 const WordsList = () => {
-  const [words, setWords] = useState([{ id: 0, title: "", mean: "", level: "" }])
+  const [words, setWords] = useState([{ id: "", title: "", mean: "", level: "" }])
+  const [loading, setLoading] = useState(false)
   const showWords = async (level: string) => {
+    setLoading(true)
     console.log(level)
-    console.log("-------------level----------------")
     const res = await fetch("/api/selectWord", {
       method: "POST", body: JSON.stringify({
         level
       })
     })
+    if (!res.ok) {
+      throw new Error("connection failed!")
+    } else {
+      setLoading(false)
+    }
     const data = await res.json()
     console.log(data)
     setWords(data)
@@ -23,7 +29,7 @@ const WordsList = () => {
       <div className="selector">
         <p className="text-2xl py-8">Wähle dein Niveau aus!</p>
         <select onChange={(e) => showWords(e.target.value)} className="border-2 rounded-3xl text-2xl p-2 min-w-24 text-center">
-          <option value={""} disabled>Auswählen</option>
+          <option value={""} >Auswählen</option>
           <option value={"a1"}>a1</option>
           <option value={"a2"}>a2</option>
           <option value={"b1"}>b1</option>
@@ -32,13 +38,13 @@ const WordsList = () => {
         </select>
       </div>
       <div className="flex flex-col gap-12 p-12 ">
-        {words.map(w => <div key={w.id}>
+        {!loading ? words[0].id && words.map(w => <div key={w.id}>
           <div className="border-2 rounded-3xl p-4 text-xl flex gap-6 flex-col">
             <p className="">  <span className="text-yellow-500">Niveau :</span>     <span className="text-2xl"> {w.level} </span>  </p>
             <p className="">  <span className="text-yellow-500">Wort :</span>       <span className="text-2xl"> {w.title} </span>  </p>
             <p className="">  <span className="text-yellow-500">Bedeutung :</span>  <span className="text-2xl"> {w.mean} </span>  </p>
           </div>
-        </div>)}
+        </div>) : <div className="text-center text-3xl text-cyan-700">Loading...</div>}
       </div>
     </div>
   </>
