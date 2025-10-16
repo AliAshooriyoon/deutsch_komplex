f6abce2de1e071c68a699ea9a9872ad06b8beb77
Ali Ashooriyoon <aliashooriyoon@yahoo.com>
2025-10-16 16:10:32 +0330

    show mean words

--- app/dashboard/words/lainter/page.tsx
+++ app/dashboard/words/lainter/page.tsx
@@ -8,10 +8,11 @@ const Lainter = () => {
   const [lengthBack, setLengthBack] = useState(0);
   const [prevLocation, setPrevLocation] = useState<number[]>([0]);
   const [count, setCount] = useState({ num: 0, path: "next" });
-
+  const [showAnswer, setShowAnswer] = useState(false)
 
 
   const nextWord = () => {
+    setShowAnswer(false)
     setCount(prev => { return { num: prev.num + 1, path: "next" } })
     setLengthBack(prev => prev > 0 ? prev - 1 : prev)
     setLocation({
@@ -22,6 +23,7 @@ const Lainter = () => {
   }
 
   const previousWord = () => {
+    setShowAnswer(false)
     setCount(prev => prev.num > 0 ? { num: prev.num - 1, path: "prev" } : prev)
     setLengthBack(prev => prev < prevLocation.length && count.num > 1 ? prev + 1 : prevLocation.length - 1)
     console.log(lengthBack < prevLocation.length)
@@ -45,22 +47,28 @@ const Lainter = () => {
     console.log(location.path)
     console.log("second use effect")
   }, [location]);
-
+  const showAnswerFunc = () => {
+    setShowAnswer(prev => !prev)
+  }
   return <>
     <div className=''>
       <div className="">
         <div className="lainter_box flex items-center justify-between flex-col w-[60%]
           mt-24 bg-sky-700 rounded-3xl h-[40rem] mx-auto">
           <div className="pt-24">
-            <p className="text-5xl text-center pt-12">{words[location.page].word}</p>
+            <p className="text-5xl text-center pt-12">
+              {!showAnswer ? words[location.page].word : words[location.page].meaning}
+            </p>
             <p className="text-2xl text-center pt-6">In besonderen Haltungen wird es verwendet</p>
           </div>
           <div className='flex items-center justify-around pb-24 gap-12 flex-row-reverse'>
             <button type='button' onClick={() => nextWord()} className='border-4 rounded-full p-1 flex items-center justify-center'>
               <GrNext className="w-10 h-10" /> </button>
-            <p
+            <p onClick={showAnswerFunc}
               className="text-2xl text-center bg-emerald-700
-  px-6 py-4 rounded-3xl cursor-pointer"> Antwort sehen </p>
+  px-6 py-4 rounded-3xl cursor-pointer"> {
+                !showAnswer ? <p>Bedeutung sehen </p> : <p>Wort sehen</p>
+              } </p>
             <button type='button' onClick={() => previousWord()} className='border-4 rounded-full rotate-180 p-1 flex items-center
       justify-center'> <GrNext className="w-10 h-10" /> </button>
           </div>
