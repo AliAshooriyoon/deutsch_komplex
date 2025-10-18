57aebb38fe2513473ab62554f590de9a1351dc70
Ali Ashooriyoon <aliashooriyoon@yahoo.com>
2025-10-18 20:26:01 +0330

    some changes in route

--- app/api/createWord/route.ts
+++ app/api/createWord/route.ts
@@ -1,12 +1,12 @@
-import prisma from '@/prisma/client';
-import { NextRequest, NextResponse } from 'next/server';
-import a2Words from '@/anki_wordsA2.json';
-export const GET = async () => {
-  console.log(a2Words);
-  const newFormat = a2Words.map(i => ({ word: i.title, meaning: i.mean, level: 'a2' }));
-  // const createdWord = await prisma.lainter.createMany({
-  //   data: newFormat,
-  //   skipDuplicates: true,
-  // });
-  return NextResponse.json(newFormat);
-};
+// import prisma from '@/prisma/client';
+// import { NextRequest, NextResponse } from 'next/server';
+// import a2Words from '@/anki_wordsA2.json';
+// export const GET = async () => {
+//   console.log(a2Words);
+//   const newFormat = a2Words.map(i => ({ word: i.title, meaning: i.mean, level: 'a2' }));
+//   // const createdWord = await prisma.lainter.createMany({
+//   //   data: newFormat,
+//   //   skipDuplicates: true,
+//   // });
+//   return NextResponse.json(newFormat);
+// };
