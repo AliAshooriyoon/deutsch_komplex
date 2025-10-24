d45856f9a52895b21f3ebc7b52121be5179d20b3
Ali Ashooriyoon <aliashooriyoon@yahoo.com>
2025-10-24 19:08:39 +0330

    responsive footer

--- app/components/home-design/Footer.tsx
+++ app/components/home-design/Footer.tsx
@@ -2,22 +2,27 @@ import Link from "next/link";
 import { FaGithub } from "react-icons/fa";
 const Footer = () => {
   return <>
-    <div className='footer w-full h-52 p-4 bg-stone-900 rounded-xl flex justify-between flex-row'>
-      <div className="main_footer flex flex-col justify-start gap-8 h-full items-start">
+    <div className='footer w-full lg:py-12 max-lg:py-6 px-4 bg-stone-900 rounded-xl flex
+      justify-between max-lg:flex-col lg:flex-row max-lg:gap-8'>
+      <div className="main_footer flex flex-col justify-start 
+        lg:gap-8 max-lg:gap-4 h-full items-start">
 
-        <p className="text-3xl">Möchten Sie mit uns Kontakt knüpfen? </p>
-        <Link className="text-xl" href={'mailto:aliashooriyoon@yahoo.com'}> Geben Sie uns Bescheid! </Link>
+        <p className="lg:text-3xl">Möchten Sie mit uns Kontakt knüpfen? </p>
+        <Link className="lg:text-xl underline" href={'mailto:aliashooriyoon@yahoo.com'}>
+          Geben Sie uns Bescheid! </Link>
       </div>
-      <div className="creator_main pr-12 flex flex-col gap-8">
-        <p className="text-2xl">
-          Diese Webseite ist von Ali Ashooriyoon erstellt!
+      <div className="creator_main lg:pr-12 max-lg:pr-4">
+        <p className="lg:text-2xl lg:flex flex-row gap-2">
+          <span>Diese Webseite ist von</span>
+          <span className="flex items-center gap-2 text-cyan-600">
+            <Link className="flex flex-row gap-2 lg:text-2xl items-center justify-center"
+              href={'https://github.com/AliAshooriyoon'}>
+              <FaGithub />  Ali Ashooriyoon
+            </Link>
+          </span>
+          erstellt!
         </p>
-        <p className="flex flex-row gap-4 text-xl items-center justify-center">
-          <Link className="flex flex-row gap-2 text-xl items-center justify-center"
-            href={'https://github.com/AliAshooriyoon'}>
-            <span className=""> <FaGithub /> </span> <span>Meine Github-Seite</span>
-          </Link>
-        </p>
+
       </div>
     </div>
   </>
