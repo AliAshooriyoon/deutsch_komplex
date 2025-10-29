2ab0d68c7d5d5ab990eb42b2a9bc6ce40a513f84
Ali Ashooriyoon <aliashooriyoon@yahoo.com>
2025-10-29 17:27:02 +0330

    responsive lainter

--- app/dashboard/words/lainter/page.tsx
+++ app/dashboard/words/lainter/page.tsx
@@ -37,24 +37,25 @@ const Lainter = () => {
   return <>
     <div className=''>
       <div className="">
-        <div className="lainter_box flex items-center justify-between flex-col w-[60%]
-          mt-24 bg-sky-700 rounded-3xl h-[40rem] mx-auto">
-          <div className="pt-24">
-            <p className="text-5xl text-center pt-12">
+        <div className="lainter_box flex items-center justify-between flex-col lg:w-[60%] max-lg:w-[98%]
+          mt-24 bg-sky-700 rounded-3xl lg:h-[40rem] max-lg:h-[30rem] mx-auto">
+          <div className="lg:pt-24">
+            <p className="lg:text-5xl max-lg:text-3xl text-center pt-12">
               {!showAnswer ? words[location.page].word : words[location.page].meaning}
             </p>
-            <p className="text-2xl text-center pt-6">In besonderen Haltungen wird es verwendet</p>
+            <p className="lg:text-2xl max-lg:text-lg text-center pt-6">In besonderen Haltungen wird es verwendet</p>
           </div>
-          <div className='flex items-center justify-around pb-24 gap-12 flex-row-reverse'>
+          <div className='flex items-center justify-around lg:pb-24 max-lg:pb-12 max-lg:gap-4 lg:gap-12 flex-row-reverse'>
             <button type='button' onClick={() => nextWord()} className='border-4 rounded-full p-1 flex items-center justify-center'>
-              <GrNext className="w-10 h-10" /> </button>
+              <GrNext className="lg:w-10 lg:h-10 max-lg:w-8 max-lg:h-8" /> </button>
             <div onClick={showAnswerFunc}
-              className="text-2xl text-center bg-emerald-700
-  px-6 py-4 rounded-3xl cursor-pointer"> {
+              className={`lg:text-2xl max-lg:text-lg text-center ${!showAnswer ? "bg-emerald-700" : "bg-stone-500"}
+                  px-6 py-4 rounded-3xl cursor-pointer`}> {
                 !showAnswer ? <p>Bedeutung sehen </p> : <p>Wort sehen</p>
               } </div>
-            <button type='button' onClick={() => previousWord()} className='border-4 rounded-full rotate-180 p-1 flex items-center
-      justify-center'> <GrNext className="w-10 h-10" /> </button>
+            <button type='button' onClick={() => previousWord()}
+              className='border-4 rounded-full rotate-180 p-1 flex items-center
+      justify-center'> <GrNext className="lg:w-10 lg:h-10 max-lg:w-8 max-lg:h-8" /> </button>
           </div>
         </div>
       </div>
