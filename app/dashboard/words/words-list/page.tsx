20128198c8a5d8479e078241e1c6bbd4533a0d25
Ali Ashooriyoon <aliashooriyoon@yahoo.com>
2025-10-29 19:39:33 +0330

    responsive words list

--- app/dashboard/words/words-list/page.tsx
+++ app/dashboard/words/words-list/page.tsx
@@ -24,9 +24,10 @@ const WordsList = () => {
 
   return <>
     <div className=''>
-      <div className="selector">
+      <div className="selector pb-8">
         <p className="text-2xl py-8">Wähle dein Niveau aus!</p>
-        <select onChange={(e) => showWords(e.target.value)} className="border-2 rounded-3xl text-2xl p-2 min-w-24 text-center">
+        <select onChange={(e) => showWords(e.target.value)} className="border-2 rounded-3xl
+          text-2xl p-2 min-w-24 text-center">
           <option value={""} >Auswählen</option>
           <option value={"a1"}>a1</option>
           <option value={"a2"}>a2</option>
@@ -35,12 +36,15 @@ const WordsList = () => {
           <option value={"c1"}>c1</option>
         </select>
       </div>
-      <div className="flex flex-col gap-12 p-12 ">
+      <div className="flex flex-col gap-12 lg:p-12 max-lg:px-4 max-lg:w-full ">
         {!loading ? words[0].id && words.map(w => <div key={w.id}>
-          <div className="border-2 rounded-3xl p-4 text-xl flex gap-6 flex-col">
-            <p className="">  <span className="text-yellow-500">Niveau :</span>     <span className="text-2xl"> {w.level} </span>  </p>
-            <p className="">  <span className="text-yellow-500">Wort :</span>       <span className="text-2xl"> {w.title} </span>  </p>
-            <p className="">  <span className="text-yellow-500">Bedeutung :</span>  <span className="text-2xl"> {w.mean} </span>  </p>
+          <div className="border-2 rounded-3xl p-4 lg:text-xl flex gap-6 flex-col">
+            <p className="">  <span className="text-yellow-500">Niveau :</span>
+              <span className="lg:text-2xl max-lg:text-lg"> {w.level} </span>  </p>
+            <p className="">  <span className="text-yellow-500">Wort :</span>
+              <span className="lg:text-2xl max-lg:text-lg"> {w.title} </span>  </p>
+            <p className="">  <span className="text-yellow-500">Bedeutung :</span>
+              <span className="lg:text-2xl max-lg:text-lg"> {w.mean} </span>  </p>
           </div>
         </div>) : <div className="text-center text-3xl text-cyan-700">Loading...</div>}
       </div>
