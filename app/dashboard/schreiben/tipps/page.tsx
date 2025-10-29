b434a2883ba509ecf7e9c95bf363ef850c7cfcef
Ali Ashooriyoon <aliashooriyoon@yahoo.com>
2025-10-29 15:06:31 +0330

    responsive tipps

--- app/dashboard/schreiben/tipps/page.tsx
+++ app/dashboard/schreiben/tipps/page.tsx
@@ -1,13 +1,8 @@
 const TippsPage = () => {
-  return <>
-    <div className="">
-      <div className='a1 flex flex-col gap-12'>
-        <h2 className="text-3xl text-center">Tipps im Niveau A1</h2>
-        <span className="text-xl">Textlänge</span> <span className="text-xl">ca. 30–40 Wörter.</span>
-        <p className="text-2xl">Aufgabenarten: einfache E-Mails, Postkarten, kurze Notizen.</p>
-        <p className='text-2xl'>
-          Tipps:
-          Immer Begrüßung und Verabschiedung schreiben: „Hallo…“ / „Liebe…“ – „Viele Grüße“.
+  const tipps = [
+    {
+      level: "a1", textlang: "ca. 30–40 Wörter",
+      tasks: 'einfache E-Mails, Postkarten, kurze Notizen.', tipps: `Immer Begrüßung und Verabschiedung schreiben: „Hallo…“ / „Liebe…“ – „Viele Grüße“.
           3–4 kurze Sätze mit sehr einfachem Wortschatz.
           Häufige Themen: Freizeit, Familie, Essen, Wohnen.
           Beispielstruktur für eine E-Mail:
@@ -15,29 +10,20 @@ const TippsPage = () => {
           Grund („Ich schreibe dir…“)
           2–3 Infos (z. B. Einladung, Termin, Uhrzeit)
           Grußformel
-        </p>
-      </div>
-      <div className='pt-28'>
-        <h2 className="text-3xl text-center">Tipps im Niveau A2</h2>
-        <p className="text-2xl">Textlänge: ca. 50–80 Wörter.</p>
-        <p className='text-2xl'>Aufgabenarten: private Briefe, einfache E-Mails, kleine Beschreibungen.</p>
-        <p className="text-2xl">
-          Tipps:
-          Immer Einleitung – Hauptteil – Schluss.
+` },
+    {
+      level: "a2", textlang: ": ca. 50–80 Wörter.",
+      tasks: 'private Briefe, einfache E-Mails, kleine Beschreibungen.', tipps: `Immer Einleitung – Hauptteil – Schluss.
           Wichtige Redemittel: „Zuerst… Dann… Danach… Schließlich…“
           Wörter für persönliche Meinungen: „Ich finde…“, „Meiner Meinung nach…“.
           Zeitangaben benutzen: „Am Montag… um 15 Uhr…“.
           Rechtschreibung zählt! Lieber kurze klare Sätze.
           Themen: Urlaub, Arbeit, Gesundheit, Stadt, Einkaufen.
-        </p>
-      </div>
-      <div className='pt-28'>
-        <h2 className="text-3xl text-center">Tipps im Niveau B1</h2>
-        <p className="text-2xl">Textlänge: ca. 100–120 Wörter.</p>
-        <p className='text-2xl'>Aufgabenarten: Leserbriefe, E-Mails an eine Redaktion, kurze Stellungnahmen.</p>
-        <p className="text-2xl">
-          Tipps:
-          Immer Aufgabe genau lesen → alle Punkte abdecken!
+` },
+    {
+      level: "b1", textlang: "ca. 100–120 Wörter.",
+      tasks: 'Leserbriefe, E-Mails an eine Redaktion, kurze Stellungnahmen.',
+      tipps: `Immer Aufgabe genau lesen → alle Punkte abdecken!
           Struktur:
           Einleitung (z. B. Bezug auf die Aufgabe: „Ich habe Ihren Artikel gelesen…“)
           Hauptteil (Meinung + Begründung + Beispiele)
@@ -48,14 +34,11 @@ const TippsPage = () => {
           „Das finde ich wichtig, weil…“
           Tipp: Mindestens ein Beispiel geben! („Zum Beispiel…“)
           Wichtig: klarer roter Faden, kein Chaos.
-        </p>
-      </div>
-      <div className='pt-28'>
-        <h2 className="text-3xl text-center">Tipps im Niveau B2</h2>
-        <p className="text-2xl">Textlänge: ca. 150–180 Wörter.</p>
-        <p className='text-2xl'>Aufgabenarten: formelle Briefe, Stellungnahmen, kurze Aufsätze, Zusammenfassungen.</p>
-        <p className="text-2xl">
-          Klare Gliederung mit Absätzen!
+` },
+    {
+      level: "b2", textlang: "ca. 150–180 Wörter.",
+      tasks: 'formelle Briefe, Stellungnahmen, kurze Aufsätze, Zusammenfassungen.',
+      tipps: `Klare Gliederung mit Absätzen!
           Immer Argumente mit Begründung und Beispielen.
           Redemittel:
           „Im Gegensatz dazu…“
@@ -64,15 +47,10 @@ const TippsPage = () => {
           Formeller Stil bei Briefen: „Sehr geehrte Damen und Herren, … Mit freundlichen Grüßen“.
           Zusammenfassung schreiben: nur die Hauptpunkte, keine Details.
           Fehler vermeiden: lieber kurze klare Sätze als zu kompliziert.
-          Tipp: Synonyme nutzen → nicht immer „gut“ oder „schlecht“, sondern auch „positiv/negativ“, „vorteilhaft/nachteilig“.        </p>
-      </div>
-      <div className='pt-28'>
-        <h2 className="text-3xl text-center">Tipps im Niveau C1</h2>
-        <p className="text-2xl">Textlänge: ca. 250 Wörter.</p>
-        <p className='text-2xl'>Aufgabenarten: Essay, Kommentar, Diskussion, Analyse.</p>
-        <p className="text-2xl">
-          Tipps:
-          Sehr klare Gliederung:
+          Tipp: Synonyme nutzen → nicht immer „gut“ oder „schlecht“, sondern auch „positiv/negativ“, „vorteilhaft/nachteilig“. ` },
+    {
+      level: "c1", textlang: " ca. 250 Wörter.",
+      tasks: ' Essay, Kommentar, Diskussion, Analyse.', tipps: `Sehr klare Gliederung:
           Einleitung (Thema vorstellen, Fragestellung formulieren)
           Hauptteil (Pro und Contra, verschiedene Perspektiven, Belege)
           Schluss (eigene Stellungnahme, Fazit, ggf. Lösungsvorschlag)
@@ -84,8 +62,28 @@ const TippsPage = () => {
           Quellen / Fakten richtig einbauen: „Laut einer Studie…“
           Auf sprachliche Vielfalt achten: komplexe Sätze, Konnektoren („obwohl, während, sodass…“).
           Sprachregister beachten: in C1 keine Umgangssprache!
-        </p>
-      </div>
+` },
+
+  ]
+  return <>
+    <div className="max-lg:px-2">
+
+      {tipps.map(i => {
+        return <div key={i.level} className='py-4 flex flex-col gap-12'>
+          <h2 className="text-3xl text-center">Tipps im Niveau {i.level.toUpperCase()}</h2>
+          <div className="flex">
+            <span className="text-xl">Textlänge : </span>
+            <span className="text-lg"> {i.textlang} </span>
+          </div>
+          <p className="lg:text-2xl max-lg:text-xl">Aufgabenarten :
+            <span className="lg:text-xl max-lg:text-lg">
+              <span>  </span> {i.tasks}  </span> </p>
+          <p className='lg:text-2xl max-lg:text-lg'>
+            <span className="text-xl">Tipps : </span>
+            <span className="text-lg">{i.tipps}</span>
+          </p>
+        </div>
+      })}
     </div>
   </>
 }
