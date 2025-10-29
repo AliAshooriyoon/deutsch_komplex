00754c558241f4246528648cf47ee7eaff9dd635
Ali Ashooriyoon <aliashooriyoon@yahoo.com>
2025-10-29 18:19:15 +0330

    responsive account page

--- app/dashboard/account/page.tsx
+++ app/dashboard/account/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 import { signOut, useSession } from "next-auth/react";
+import Link from "next/link";
 
 const Account = () => {
   const user = useSession();
@@ -9,15 +10,17 @@ const Account = () => {
   }
   return <>
     <div className=''>
-      <div className="">
+      <div className="max-lg:flex flex-col items-center gap-12">
         <p className='user text-3xl text-center'>Hallo {user.data?.user?.name}!</p>
         <button onClick={() => logOutAction()}
-          className='rounded-2xl py-3 px-7 text-2xl bg-red-700 border-2 cursor-pointer'>SignOut</button>
+          className='rounded-2xl max-lg:w-[50%] py-3 px-7 text-2xl 
+          bg-red-700 border-2 cursor-pointer'>SignOut</button>
       </div>
       <div className="py-10 px-4 text-xl">
         Hast du jede Probleme ?
         <p>Gib uns Bescheid über dein Problem!</p>
-        <span className="text-cyan-600">aliashooriyoon@yahoo.com</span>
+        <Link href={"mailto:aliashooriyoon@yahoo.com"} className="text-cyan-600">
+          aliashooriyoon@yahoo.com</Link>
       </div>
     </div>
   </>
