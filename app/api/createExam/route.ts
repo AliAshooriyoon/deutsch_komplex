9edeb971507fe116f8a2fbed08e2f00c53e6a857
Ali Ashooriyoon <aliashooriyoon@yahoo.com>
2025-11-02 23:30:03 +0330

    change referer

--- app/api/createExam/route.ts
+++ app/api/createExam/route.ts
@@ -5,7 +5,7 @@ const openai = new OpenAI({
   apiKey: process.env.OPENROUTER_API_KEY,
   baseURL: 'https://openrouter.ai/api/v1',
   defaultHeaders: {
-    'HTTP-Referer': 'http://localhost:3000',
+    'HTTP-Referer': process.env.NEXTAUTH_URL,
     'X-Title': 'deutsch komplex',
   },
 });
