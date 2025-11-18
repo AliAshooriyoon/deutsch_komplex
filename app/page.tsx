cd07e5780035dcfdbd064564760601752d5fff15
Ali Ashooriyoon <aliashooriyoon@yahoo.com>
2025-11-18 17:33:07 +0330

    design services part

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 "use client"
 import Image from "next/image";
-import { FaUser } from "react-icons/fa";
-import { FaBookOpen } from "react-icons/fa";
-import { PiExamFill } from "react-icons/pi";
+import { GoBook } from "react-icons/go";
 import mainPic from "../public/team-university-colleagues-are-doing-research-class-project.jpg"
 import customPic from "../public/removebg.png"
+import { VscComment } from "react-icons/vsc";
+import { AiFillThunderbolt } from "react-icons/ai";
+import { PiBrain } from "react-icons/pi";
 import Plan from "./components/home-design/plan";
 import Footer from "./components/home-design/Footer";
 import Header from "./components/Header";
@@ -60,34 +61,60 @@ export default function Home() {
           </div>
           <Image alt="main picture" draggable={false} className="w-[40rem] rounded-3xl" src={mainPic} />
         </div>
-        <Element name="services" className="analys p-6 lg:h-44  shadow-2xl
-          shadow-[#D73722] w-[96%] mx-auto
-          bg-zinc-900 rounded-4xl flex max-lg:flex-col max-lg:gap-16 justify-between">
-          <div className='users flex items-center gap-6'>
-            <div className='iconBox border-2 p-4 rounded-full bg-red-300 border-red-300'>
-              <FaUser className="max-lg:h-6 max-lg:w-6 lg:w-8 lg:h-8 fill-[#D73722]" />
+        <Element name="services" className="analys text-center">
+          <h3 className="text-4xl mt-12">Alle
+            <span className="text-red-600 "> Prüfungsteile</span> abgedeckt</h3>
+          <h5 className="text-xl pt-4 mx-auto text-gray-700">Bereite dich gezielt auf alle vier Fertigkeiten vor und meistere
+            deine <br /> Deutschprüfung mit Selbstvertrauen.</h5>
+          <div className="items_services pt-12 text-start w-[70%] gap-y-10 mx-auto flex justify-between flex-wrap">
+            <div className="w-[30%] h-48 border-2 border-gray-400 p-4
+              flex flex-col justify-between rounded-2xl">
+              <div className="w-12 h-12 bg-red-400/40 flex justify-center items-center rounded-2xl">
+                <GoBook className="text-red-600 w-8 h-8" />
+              </div>
+              <p className="text-black text-xl">Leseverstehen</p>
+              <p className="text-gray-600">Trainiere mit authentischen Texten und prüfungsrelevanten
+                Aufgabenformaten für alle Sprachniveaus.</p>
             </div>
-            <div className='nums'>
-              <p className='lg:text-3xl max-lg:text-2xl font-bold'>Über 200</p>
-              <p className='text-xl'>Benutzer</p>
+            <div className="w-[30%] h-48 border-2 border-gray-400 p-4
+              flex flex-col justify-between rounded-2xl">
+              <div className="w-12 h-12 bg-gray-500/40 flex justify-center items-center rounded-2xl">
+                <VscComment className="text-gray-900 w-8 h-8" />
+              </div>
+              <p className="text-black text-xl">Mündlicher Ausdruck</p>
+              <p className="text-gray-600">Übe Sprechen mit realistischen Prüfungssituationen und erhalte detailliertes Feedback.</p>
             </div>
-          </div>
-          <div className='practices flex items-center gap-6'>
-            <div className="iconBox border-2 p-4 rounded-full bg-red-300 border-red-300">
-              <FaBookOpen className="max-lg:h-6 max-lg:w-6 lg:w-8 lg:h-8 fill-[#D73722]" />  </div>
-            <div className='nums'>
-              <p className='lg:text-3xl max-lg:text-2xl font-bold'>Über 130</p>
-              <p className='text-xl'>Übungen</p>
+            <div className="w-[30%] h-48 border-2 border-gray-400 p-4
+              flex flex-col justify-between rounded-2xl">
+              <div className="w-12 h-12 bg-yellow-400/40 flex justify-center items-center rounded-2xl">
+                <AiFillThunderbolt className="text-yellow-600 w-8 h-8" />
+              </div>
+              <p className="text-black text-xl">Hörverstehen</p>
+              <p className="text-gray-600">Verbessere dein Hörverständnis mit gezielten Übungen und originalen Prüfungsformaten.</p>
             </div>
-          </div>
-
-          <div className='Modell-Tests flex items-center gap-6'>
-            <div className="iconBox border-2 p-4 rounded-full bg-red-300 border-red-300">
-              <PiExamFill className="max-lg:h-6 max-lg:w-6 lg:w-8 lg:h-8 fill-[#D73722]" />
+            <div className="w-[30%] h-48 border-2 border-gray-400 p-4
+              flex flex-col justify-between rounded-2xl">
+              <div className="w-12 h-12 bg-red-400/40 flex justify-center items-center rounded-2xl">
+                <PiBrain className="text-red-600 w-8 h-8" />
+              </div>
+              <p className="text-black text-xl">Schriftlicher Ausdruck</p>
+              <p className="text-gray-600">Lerne Essays, Briefe und Berichte zu schreiben – genau wie in der Prüfung.</p>
             </div>
-            <div className='nums'>
-              <p className='lg:text-3xl max-lg:text-2xl font-bold'>Über 40</p>
-              <p className='text-xl'>Modell-Tests</p>
+            <div className="w-[30%] h-48 border-2 border-gray-400 p-4
+              flex flex-col justify-between rounded-2xl">
+              <div className="w-12 h-12 bg-gray-500/40 flex justify-center items-center rounded-2xl">
+                <GoBook className="text-gray-900 w-8 h-8" />
+              </div>
+              <p className="text-black text-xl">Prüfungssimulation</p>
+              <p className="text-gray-600">Teste dich unter echten Prüfungsbedingungen und kenne dein Niveau.</p>
+            </div>
+            <div className="w-[30%] h-48 border-2 border-gray-400 p-4
+              flex flex-col justify-between rounded-2xl">
+              <div className="w-12 h-12 bg-yellow-400/40 flex justify-center items-center rounded-2xl">
+                <GoBook className="text-yellow-700 w-8 h-8" />
+              </div>
+              <p className="text-black text-xl"> {`Grammatik & Wortschatz`} </p>
+              <p className="text-gray-600">Erweitere systematisch deinen Wortschatz und beherrsche die deutsche Grammatik.</p>
             </div>
           </div>
         </Element>
