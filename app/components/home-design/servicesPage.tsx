1b06581094c283f3246d21a2919c20c29ed878c0
Ali Ashooriyoon <aliashooriyoon@yahoo.com>
2025-11-19 15:20:39 +0330

    responsive services part

--- app/components/home-design/servicesPage.tsx
+++ app/components/home-design/servicesPage.tsx
@@ -10,12 +10,15 @@ const Element = dynamic(() => import('react-scroll').then((mod) => mod.Element),
 const ServicesPage = () => {
   return <>
     <Element name="services" className="analys bg-white text-center py-12">
-      <h3 className="text-4xl mt-12">Alle
-        <span className="text-red-600 "> Prüfungsteile</span> abgedeckt</h3>
-      <h5 className="text-xl pt-4 mx-auto text-gray-700">Bereite dich gezielt auf alle vier Fertigkeiten vor und meistere
-        deine <br /> Deutschprüfung mit Selbstvertrauen.</h5>
-      <div className="items_services pt-12 text-start w-[70%] gap-y-10 mx-auto flex justify-between flex-wrap">
-        <div className="w-[30%] h-48 border-2 border-gray-400 p-4
+      <h3 className="lg:text-4xl max-lg:text-3xl mt-12">Alle
+        <span className="text-red-600"> Prüfungsteile</span> abgedeckt</h3>
+      <h5 className="lg:text-xl max-lg:text-lg pt-4 mx-auto text-gray-700">
+        Bereite dich gezielt auf alle vier Fertigkeiten vor und meistere
+        deine <br /> Deutschprüfung mit Selbstvertrauen.
+      </h5>
+      <div className="items_services pt-12 text-start lg:w-[70%] max-lg:w-[95%] gap-y-10
+        mx-auto flex justify-between flex-wrap">
+        <div className="lg:w-[30%] lg:h-48 border-2 border-gray-400 p-4
               flex flex-col justify-between rounded-2xl">
           <div className="w-12 h-12 bg-red-400/40 flex justify-center items-center rounded-2xl">
             <GoBook className="text-red-600 w-8 h-8" />
@@ -24,7 +27,7 @@ const ServicesPage = () => {
           <p className="text-gray-600">Trainiere mit authentischen Texten und prüfungsrelevanten
             Aufgabenformaten für alle Sprachniveaus.</p>
         </div>
-        <div className="w-[30%] h-48 border-2 border-gray-400 p-4
+        <div className="lg:w-[30%] h-48 border-2 border-gray-400 p-4
               flex flex-col justify-between rounded-2xl">
           <div className="w-12 h-12 bg-gray-500/40 flex justify-center items-center rounded-2xl">
             <VscComment className="text-gray-900 w-8 h-8" />
@@ -32,7 +35,7 @@ const ServicesPage = () => {
           <p className="text-black text-xl">Mündlicher Ausdruck</p>
           <p className="text-gray-600">Übe Sprechen mit realistischen Prüfungssituationen und erhalte detailliertes Feedback.</p>
         </div>
-        <div className="w-[30%] h-48 border-2 border-gray-400 p-4
+        <div className="lg:w-[30%] h-48 border-2 border-gray-400 p-4
               flex flex-col justify-between rounded-2xl">
           <div className="w-12 h-12 bg-yellow-400/40 flex justify-center items-center rounded-2xl">
             <AiFillThunderbolt className="text-yellow-600 w-8 h-8" />
@@ -40,7 +43,7 @@ const ServicesPage = () => {
           <p className="text-black text-xl">Hörverstehen</p>
           <p className="text-gray-600">Verbessere dein Hörverständnis mit gezielten Übungen und originalen Prüfungsformaten.</p>
         </div>
-        <div className="w-[30%] h-48 border-2 border-gray-400 p-4
+        <div className="lg:w-[30%] h-48 border-2 border-gray-400 p-4
               flex flex-col justify-between rounded-2xl">
           <div className="w-12 h-12 bg-red-400/40 flex justify-center items-center rounded-2xl">
             <PiBrain className="text-red-600 w-8 h-8" />
@@ -48,7 +51,7 @@ const ServicesPage = () => {
           <p className="text-black text-xl">Schriftlicher Ausdruck</p>
           <p className="text-gray-600">Lerne Essays, Briefe und Berichte zu schreiben – genau wie in der Prüfung.</p>
         </div>
-        <div className="w-[30%] h-48 border-2 border-gray-400 p-4
+        <div className="lg:w-[30%] h-48 border-2 border-gray-400 p-4
               flex flex-col justify-between rounded-2xl">
           <div className="w-12 h-12 bg-gray-500/40 flex justify-center items-center rounded-2xl">
             <GoBook className="text-gray-900 w-8 h-8" />
@@ -56,7 +59,7 @@ const ServicesPage = () => {
           <p className="text-black text-xl">Prüfungssimulation</p>
           <p className="text-gray-600">Teste dich unter echten Prüfungsbedingungen und kenne dein Niveau.</p>
         </div>
-        <div className="w-[30%] h-48 border-2 border-gray-400 p-4
+        <div className="lg:w-[30%] h-48 border-2 border-gray-400 p-4
               flex flex-col justify-between rounded-2xl">
           <div className="w-12 h-12 bg-yellow-400/40 flex justify-center items-center rounded-2xl">
             <GoBook className="text-yellow-700 w-8 h-8" />
