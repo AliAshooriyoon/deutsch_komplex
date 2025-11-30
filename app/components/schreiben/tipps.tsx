7a575396170c2cfdcd02f52f7bd681931b67d631
Ali Ashooriyoon <aliashooriyoon@yahoo.com>
2025-11-30 20:41:26 +0330

    responsive schreiben tipps

--- app/components/schreiben/tipps.tsx
+++ app/components/schreiben/tipps.tsx
@@ -1,13 +1,13 @@
 const TippsComponent = () => {
-  const classes = [`w-[45%] h-[50%] bg-gradient-to-r
-          from-red-500/20 to-amber-500/20 p-4 rounded-2xl flex flex-col gap-4 justify-between`,
+  const classes = [`lg:w-[45%]  bg-gradient-to-r
+          from-red-500/20 to-amber-500/20 p-6 rounded-2xl flex flex-col gap-4 justify-between`,
     `flex items-center gap-2 text-xl`, `w-8 h-8 flex items-center justify-center rounded-lg text-lg
             bg-red-600 text-white`
   ]
   return <>
     <div className="w-full bg-white border-2 border-gray-300 rounded-2xl p-4">
       <p className="header-tipps text-2xl"> Wichtige Schreibtipps</p>
-      <div className="w-full h-full p-6 flex justify-around flex-wrap gap-y-12">
+      <div className="w-full h-full p-6 flex justify-around max-lg:flex-col flex-wrap gap-y-12">
         <div className={classes[0]}>
           <div className={classes[1]}>
             <div className={classes[2]}>1</div>
@@ -48,7 +48,7 @@ const TippsComponent = () => {
       </div>
     </div>
 
-    <div className="w-full h-64 bg-gray-900 text-white  my-12 rounded-3xl p-6">
+    <div className="w-full ah-64 bg-gray-900 text-white  my-12 rounded-3xl p-6">
       <p className="title__explain text-2xl pb-4">Häufige Fehler vermeiden</p>
       <ul className="list-disc pl-6 leading-9">
         <li className=""> Zu lange Sätze – halte Sätze kurz und präzise </li>
