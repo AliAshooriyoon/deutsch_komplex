21bb8b7099cf30ad17534a6c5885468183f1d6d2
Ali Ashooriyoon <aliashooriyoon@yahoo.com>
2025-12-12 00:18:52 +0330

    remove sign-up button from account component

--- app/dashboard/account/page.tsx
+++ app/dashboard/account/page.tsx
@@ -1,20 +1,15 @@
 "use client"
-import { signOut, useSession } from "next-auth/react";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 
 const Account = () => {
   const user = useSession();
   console.log(user)
-  const logOutAction = () => {
-    signOut({ callbackUrl: "/login" })
-  }
+
   return <>
     <div className=''>
       <div className="max-lg:flex flex-col items-center gap-12">
         <p className='user text-3xl text-center'>Hallo {user.data?.user?.name}!</p>
-        <button onClick={() => logOutAction()}
-          className='rounded-2xl max-lg:w-[50%] py-3 px-7 text-2xl 
-          bg-red-700 border-2 cursor-pointer'>SignOut</button>
       </div>
       <div className="py-10 px-4 text-xl">
         Hast du jede Probleme ?
