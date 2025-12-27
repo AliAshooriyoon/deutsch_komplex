13aa025cddcca2a7325b84fb2aabd2d55720f6c5
Ali Ashooriyoon <aliashooriyoon@yahoo.com>
2025-12-27 23:28:18 +0330

    add filter for level of examples in Schreiben

--- app/components/schreiben/examples.tsx
+++ app/components/schreiben/examples.tsx
@@ -4,6 +4,8 @@ import { useEffect, useState } from "react";
 import ExampleSchreiben from "./example";
 
 const ExamplesSchreiben = () => {
+  const [filter, setFilter] = useState("")
+  const [showData, setShowData] = useState<Array<{ id: number, level: string, exam: string, topic: string, content: string }>>()
   const [data, setData] = useState<Array<{ id: number, level: string, exam: string, topic: string, content: string }>>([
     {
       id: 0, level: "", exam: "", topic: "",
@@ -23,6 +25,7 @@ const ExamplesSchreiben = () => {
     const data = await res.json();
     if (res.ok) {
       setData(data)
+      setShowData(data)
       setIsLoadnig(false)
     } else {
       return <p className="text-red-500 text-center text-4xl">Connecting error!</p>
@@ -32,11 +35,31 @@ const ExamplesSchreiben = () => {
     examplesShow()
   }, [])
 
-  return <>
-    <div className="flex items-center justify-between flex-wrap gap-12 bg-wahite
-      max-lg:px-4 lg:px-8py-12 h-full">
-      {!isLoading ? data?.map(i => <ExampleSchreiben key={i.id} item={i} />) : <p className="text-red-500 text-2xl">Loading...</p>}
+  useEffect(() => {
+    setShowData(prev => filter != "" ? data.filter((i) => i.level == filter) : data)
+  }, [filter])
 
+  return <>
+    <div className="">
+      <div className="flex items-center gap-4 p-8">
+        <p className="lg:text-xl pl-4">Wähle das Niveau aus</p>
+        <select className="p-4  rounded-2xl lg:text-xl" onChange={(e) => setFilter(e.target.value)}>
+          <option value={""}>Alle</option>
+          <option value={"a1"}>A1</option>
+          <option value={"a2"}>A2</option>
+          <option value={"b1"}>B1</option>
+          <option value={"b2"}>B2</option>
+          <option value={"c1"}>C1</option>
+        </select>
+
+
+      </div>
+      <div className="flex items-center justify-between flex-wrap gap-12 bg-wahite
+      max-lg:px-4 lg:px-8py-12 h-full">
+        {!isLoading ? showData?.map(i => <ExampleSchreiben key={i.id} item={i} />) : <p className="text-red-500 text-2xl">Loading...</p>}
+        {/* {!isLoading ? data?.map(i => <ExampleSchreiben key={i.id} item={i} />) : <p className="text-red-500 text-2xl">Loading...</p>} */}
+
+      </div>
     </div>
   </>
 }
