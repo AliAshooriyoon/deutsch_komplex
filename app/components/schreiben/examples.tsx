ee3d889d16972e1b47ee90caf10b9e9d25ba4bd1
Ali Ashooriyoon <aliashooriyoon@yahoo.com>
2025-12-27 23:47:44 +0330

    add filter for exam Center of examples in Schreiben

--- app/components/schreiben/examples.tsx
+++ app/components/schreiben/examples.tsx
@@ -4,7 +4,8 @@ import { useEffect, useState } from "react";
 import ExampleSchreiben from "./example";
 
 const ExamplesSchreiben = () => {
-  const [filter, setFilter] = useState("")
+  const [filterLevel, setFilterLevel] = useState("")
+  const [filterCenter, setFilterCenter] = useState("")
   const [showData, setShowData] = useState<Array<{ id: number, level: string, exam: string, topic: string, content: string }>>()
   const [data, setData] = useState<Array<{ id: number, level: string, exam: string, topic: string, content: string }>>([
     {
@@ -36,23 +37,38 @@ const ExamplesSchreiben = () => {
   }, [])
 
   useEffect(() => {
-    setShowData(prev => filter != "" ? data.filter((i) => i.level == filter) : data)
-  }, [filter])
+    setShowData(prev => filterLevel != "" && filterCenter != "" ? data.filter((i) => (i.level == filterLevel && i.exam == filterCenter)) :
+      filterLevel != "" ? data.filter((i) => i.level == filterLevel) : filterCenter != "" ? data.filter((i) => i.exam == filterCenter) : data)
+  }, [filterLevel, filterCenter])
 
   return <>
     <div className="">
-      <div className="flex items-center gap-4 p-8">
-        <p className="lg:text-xl pl-4">Wähle das Niveau aus</p>
-        <select className="p-4  rounded-2xl lg:text-xl" onChange={(e) => setFilter(e.target.value)}>
-          <option value={""}>Alle</option>
-          <option value={"a1"}>A1</option>
-          <option value={"a2"}>A2</option>
-          <option value={"b1"}>B1</option>
-          <option value={"b2"}>B2</option>
-          <option value={"c1"}>C1</option>
-        </select>
+      <div className="flex w-full justify-around">
+        <div className="flex items-center gap-4 p-8">
+          <p className="lg:text-xl pl-4">Wähle das Niveau aus</p>
+          <select className="p-4  rounded-2xl lg:text-xl" onChange={(e) => setFilterLevel(e.target.value)}>
+            <option value={""}>Alle</option>
+            <option value={"a1"}>A1</option>
+            <option value={"a2"}>A2</option>
+            <option value={"b1"}>B1</option>
+            <option value={"b2"}>B2</option>
+            <option value={"c1"}>C1</option>
+          </select>
 
 
+        </div>
+        <div className="flex items-center gap-4 p-8">
+          <p className="lg:text-xl pl-4">Wähle das PrüfungZentrum aus</p>
+          <select className="p-4  rounded-2xl lg:text-xl" onChange={(e) => setFilterCenter(e.target.value)}>
+            <option value={""}>Alle</option>
+            <option value={"ÖSD"}>ÖSD</option>
+            <option value={"TELC"}>TELC</option>
+            <option value={"GOETHE"}>GOETHE</option>
+          </select>
+
+
+
+        </div>
       </div>
       <div className="flex items-center justify-between flex-wrap gap-12 bg-wahite
       max-lg:px-4 lg:px-8py-12 h-full">
