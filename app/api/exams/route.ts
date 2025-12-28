3d4db657aba643cb5c6d32c86a49cf23841f3add
Ali Ashooriyoon <aliashooriyoon@yahoo.com>
2025-12-28 13:40:21 +0330

    change exams route

--- app/api/exams/route.ts
+++ app/api/exams/route.ts
@@ -1,7 +1,12 @@
+import { auth } from '@/auth';
 import prisma from '@/prisma/client';
 import { NextRequest, NextResponse } from 'next/server';
 
 export const GET = async () => {
+  const session = await auth();
+  if (!session?.user || !session.user.email) {
+    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+  }
   const data = await prisma.exam.findMany();
   return NextResponse.json(data);
 };
