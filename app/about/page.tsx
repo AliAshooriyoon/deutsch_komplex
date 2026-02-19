e8fa4251d68f6099976f906e6a810060a73a85c3
Ali Ashooriyoon <aliashooriyoon@yahoo.com>
2026-02-19 23:33:48 +0330

    edit about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,10 @@
 const About = () => {
   return <>
-    <div className=''>
-      <h1 className="text-4xl py-16">
+    <div className='py-64 bg-gradient-to-r from-red-500/80 to-amber-500/80 flex items-center justify-center'>
+      <h1 className="text-4xl text-center">
         Über Deutsch-Komplex
       </h1>
+      <h2 className="text-2xl py-4">Wir helfen Menschen weltweit dabei, ihre deutschen Sprachprüfungen erfolgreich zu bestehen</h2>
     </div>
   </>
 }
