ecfe3018114cb33ce5c622283e08cd32cd257eb4
Ali Ashooriyoon <aliashooriyoon@yahoo.com>
2026-02-19 23:48:15 +0330

    edit about page text styles

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,7 @@
 const About = () => {
   return <>
-    <div className='py-52 bg-gradient-to-r from-red-700/90 to-amber-600/80 gap-12 flex items-center justify-center flex-col'>
-      <h1 className="text-6xl text-center max-lg:text-3xl">
+    <div className='py-52 bg-gradient-to-r from-red-800 to-amber-600 gap-12 flex items-center justify-center flex-col'>
+      <h1 className="text-7xl text-center max-lg:text-3xl text-white">
         Über Deutsch-Komplex
       </h1>
       <h2 className="text-3xl max-lg:text-xl max-lg:px-4">Wir helfen Menschen weltweit dabei, ihre deutschen Sprachprüfungen erfolgreich zu bestehen</h2>
