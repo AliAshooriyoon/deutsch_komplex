926fea1dc501817c9b229adb2f787054e1984a85
Ali Ashooriyoon <aliashooriyoon@yahoo.com>
2026-02-19 23:40:45 +0330

    edit about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,7 +4,7 @@ const About = () => {
       <h1 className="text-6xl text-center max-lg:text-3xl">
         Über Deutsch-Komplex
       </h1>
-      <h2 className="text-3xl max-lg:text-xl">Wir helfen Menschen weltweit dabei, ihre deutschen Sprachprüfungen erfolgreich zu bestehen</h2>
+      <h2 className="text-3xl max-lg:text-xl max-lg:px-4">Wir helfen Menschen weltweit dabei, ihre deutschen Sprachprüfungen erfolgreich zu bestehen</h2>
     </div>
   </>
 }
