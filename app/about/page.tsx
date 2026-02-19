5b2dec02fdb7440056209d21523632013c10732d
Ali Ashooriyoon <aliashooriyoon@yahoo.com>
2026-02-19 23:56:22 +0330

    edit about page text styles

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,7 @@
 const About = () => {
   return <>
-    <div className='py-52 bg-gradient-to-r from-red-800/90 to-amber-600/20 gap-12 flex items-center justify-center flex-col'>
+    <div className='py-52 bg-gradient-to-r from-red-700 from-[0%]  via-red-700 via-[70%]
+      to-amber-600 to-[100%] gap-12 flex items-center justify-center flex-col'>
       <h1 className="text-7xl text-center max-lg:text-3xl text-white">
         Über Deutsch-Komplex
       </h1>
