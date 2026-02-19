a03f2c5611f65e44058897262ea36f4447b252b7
Ali Ashooriyoon <aliashooriyoon@yahoo.com>
2026-02-19 23:24:51 +0330

    edit about page

--- app/about/layout.tsx
+++ app/about/layout.tsx
@@ -3,7 +3,7 @@ import Header from "../components/Header";
 const AboutLayout = ({ children }: { children: React.ReactNode }) => {
   return <>
     <Header />
-    {children}
+    <div className="pt-28">{children}</div>
   </>
 }
 export default AboutLayout;
