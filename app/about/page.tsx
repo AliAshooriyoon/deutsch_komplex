4c2b80e354bad1bfbba77091c1f0579dd43b9edd
Ali Ashooriyoon <aliashooriyoon@yahoo.com>
2026-02-20 11:11:25 +0330

    write cotact with us for edit icons

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -61,20 +61,20 @@ const About = () => {
       <div className="">
         <h2 className="text-5xl text-center">Kontaktiere uns</h2>
         <div className="flex gap-10 w-full justify-center ">
-          <div className="h-48 bg-gray-900 px-20">
-            <div className="bg-red-600 p-2"><MdOutlineEmail className="w-6 h-6" /></div>
+          <div className="h-48 bg-gray-900 px-20 flex flex-col gap-6">
+            <div className="bg-red-600 p-2"><MdOutlineEmail className="w-10 h-10" /></div>
             <p className="text-2xl">E-Mail</p>
             <p className="text-md">deutsch-komplex@proton.me</p>
           </div>
-          <div className="h-48 bg-gray-900 px-20">
-            <div className="bg-amber-600 p-2"><FaTelegram className="w-6 h-6" /></div>
+          <div className="h-48 bg-gray-900 px-20 flex flex-col gap-6">
+            <div className="bg-amber-600 p-2"><FaTelegram className="w-10 h-10" /></div>
             <p className="text-2xl">Telegram</p>
             <p className="text-md">deutsch-komplex@proton.me</p>
 
           </div>
-          <div className="h-48 bg-gray-900 px-20">
+          <div className="h-48 bg-gray-900 px-20 flex flex-col gap-6">
             <p className="text-2xl">GitHub</p>
-            <div className="bg-stone-900 p-2"><FaGithub className="w-6 h-6" /></div>
+            <div className="bg-stone-900 p-2"><FaGithub className="w-10 h-10" /></div>
             <Link href={"github.com/AliAshooriyoon/deutsch_komplex"} className="text-md">Github projekt</Link>
           </div>
         </div>
