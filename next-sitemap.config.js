c2d55479c045c748010c47741fb6dbbbc5dd8901
Ali Ashooriyoon <aliashooriyoon@yahoo.com>
2026-05-13 16:52:31 +0330

    add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://deutsch-komplex.com', 
+  generateRobotsTxt: true,                 
+  changefreq: 'weekly',
+  priority: 0.7,
+};
